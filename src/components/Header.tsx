7ef8483add2ca23045b149487f29a1b072a9d032
taras131 <mossnabitkana@gmail.com>
2023-05-13 13:42:08 +0300

    add my guide filter

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -44,9 +44,13 @@ const Header = () => {
                     <Navigation anchorEl={anchorEl}
                                 handleClose={handleClose}
                                 open={open}/>
+
                     <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
-                        Mechanics guide
+                        <Link to={routes.main} style={{color: "white", textDecoration: "none"}}>
+                            Mechanics guide
+                        </Link>
                     </Typography>
+
                     {isAuth && user && user.email && (
                         <Link to={routes.profile} style={{color: "white"}}>
                             <Button color="inherit">
